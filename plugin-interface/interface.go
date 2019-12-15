28c95b328ce2774db9cfc57375045b5ee7d61067
Sergio Contreras <checo1085@hotmail.com>
2019-12-15 11:47:00 -0700

    Se agrega implementacion de rpc server

--- plugin-interface/interface.go
+++ plugin-interface/interface.go
@@ -14,8 +14,21 @@ type FilterEventRPCClient struct {
 	client *rpc.Client
 }
 
-// FilterEvent filtra un evento
+// FilterEvent llama al servidor rpc para filtra un evento
 func (f *FilterEventRPCClient) FilterEvent(event []byte) (filtered []byte, err error) {
 	err = f.client.Call("Plugin.FilterEvent", event, &filtered)
 	return
+}
+
+// FilterEventRPCServer es la implementacion de rpc servidor
+type FilterEventRPCServer struct {
+	// Impl es la implementacion real de FilterEvent
+	Impl FilterEvent
+}
+
+// FilterEvent recibe un evento del servidor y lo filtra
+func (f *FilterEventRPCServer) FilterEvent(event []byte, filtered *[]byte) error {
+	v, err := f.Impl.FilterEvent(event)
+	*filtered = v
+	return err
 }
